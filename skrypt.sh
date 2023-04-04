3acc55d19173ce2c00ba01aacdfda9c86412eac7
Szymon50741 <dsw50741@student.dsw.edu.pl>
2023-04-04 23:13:35 +0200

    zad10

--- skrypt.sh
+++ skrypt.sh
@@ -21,6 +21,9 @@ if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Wyświetlanie daty -> skrypt.sh --date"
 	echo "Tworzenie 100 plikow logs -> skrypt.sh --logs"
 	echo "Tworzenie n-plikow logs -> skrypt.sh --logs n, gdzie n - liczba plikow do stworzenia"
+	echo "Sklonowanie Repozytorium i dodanie do PATH -> skrypt.sh --init"
+	echo "Tworzenie 100 plikow errorx/errorx.txt -> skrypt.sh --error, gdzie x = [1 ... 100]"
+	echo "Tworzenie n-plikow errorx/errorx.txt -> skrypt.sh --error x, gdzie x - liczba plikow do stworzenia"
 fi
 
 if [ "$1" = "--init" ]; then
