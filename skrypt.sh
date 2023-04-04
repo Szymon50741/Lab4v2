84182d8fd60b72c2450aab16eef0852d6f51c046
Szymon50741 <dsw50741@student.dsw.edu.pl>
2023-04-04 20:53:01 +0200

    zad2

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,15 @@
 if [ "$1" = "--date" ]; then
     date
 fi
+
+if [ "$1" = "--logs" ]; then
+    for i in {1..100}
+    do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: $0" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
+    done
+fi
+
+
 read -p "end"
